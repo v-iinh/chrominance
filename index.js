65bab9ec9b8342cb92b327110c56cf24904e0b8c
v-iinh <officiallyvt@gmail.com>
2024-03-30 19:25:08 -0400

    updated functionality

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-async function sayHello(){
-    let [tab] = await chrome.tabs.query({ active: true });
+async function changeBackgroundColor() {
+    let color = document.getElementById("colorPicker").value;
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
-        func: () => {
-            document.body
-            alert("Hello");
-        }
+        func: (color) => {
+            document.body.style.backgroundColor = color;
+        },
+        args: [color]
     });
 }
-document.getElementById("myButton").addEventListener("click", sayHello);
+
+async function changeTextColor() {
+    let color = document.getElementById("colorPicker1").value;
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: (color) => {
+            document.body.style.color = color;
+        },
+        args: [color]
+    });
+}
+
+async function updateTheme() {
+    let backgroundColor = document.getElementById("colorPicker").value;
+    let textColor = document.getElementById("colorPicker1").value;
+    changeBackgroundColor(backgroundColor); 
+    changeTextColor(textColor);
+}
+
+document.getElementById("backgroundButton").addEventListener("click", updateTheme);
+document.getElementById("textButton").addEventListener("click", updateTheme);
