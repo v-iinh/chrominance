2ccc37df06ab324a14d23f8e919f4b288591380e
v-iinh <officiallyvt@gmail.com>
2024-04-20 14:24:53 -0400

    updated palette

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('search').addEventListener('click', clickedSearch);
     document.getElementById('dropper').addEventListener('click', clickedDropper);
     document.getElementById('palette').addEventListener('click', clickedPalette);
     document.getElementById('contrast').addEventListener('click', clickedContrast);
+    document.getElementById('theme').addEventListener('click', clickedTheme);
+    document.getElementById('gradient').addEventListener('click', clickedGradient);
 });
 
 function resetIcons() {
     document.querySelectorAll('.nav i').forEach(icon => {
         icon.style.color = "#75797e";
+        document.querySelector('.pcr-app.visible').style.display = 'flex';
     });
-}
-
-function clickedSearch() {
-    resetIcons();
-    document.querySelector('#search i').style.color = "#4285f4";
 }
 
 function clickedDropper() {
@@ -30,15 +27,48 @@ function clickedPalette() {
     resetIcons(); 
     document.querySelector('#palette i').style.color = "#4285f4";
     let value = pickr.getColor().toHEX().toString().replace("#", "");
-    analogic(value);
+    document.querySelector('.pcr-app.visible').style.display = 'none';
+    if (!clickedPalette.modes || clickedPalette.modes.length === 0) {
+        clickedPalette.modes = ["monochrome", "monochrome-dark", "monochrome-light", "analogic", "complement", "analogic-complement", "triad", "quad"];
+        shuffleModes();
+    }
+
+    const selectedMode = clickedPalette.modes.shift();
+    if (clickedPalette.modes.length === 0) {
+        shuffleModes();
+    }
+    palette(value, selectedMode);
 }
 
-function analogic(hexColor) {
-    fetch(`https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=analogic&count=6`)
+function shuffleModes() {
+    for (let i = clickedPalette.modes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [clickedPalette.modes[i], clickedPalette.modes[j]] = [clickedPalette.modes[j], clickedPalette.modes[i]];
+    }
+}
+
+function palette(hexColor, mode) {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=${mode}&count=6`)
     .then(response => response.json())
     .then(data => {
         const colors = data.colors.map(color => color.hex.value);
-        console.log(colors.join(', '));
+        document.getElementById("one").style.backgroundColor = colors[0]
+        document.getElementById("one").innerHTML = colors[0]
+
+        document.getElementById("two").style.backgroundColor = colors[1]
+        document.getElementById("two").innerHTML = colors[1]
+
+        document.getElementById("three").style.backgroundColor = colors[2]
+        document.getElementById("three").innerHTML = colors[2]
+
+        document.getElementById("four").style.backgroundColor = colors[3]
+        document.getElementById("four").innerHTML = colors[3]
+
+        document.getElementById("five").style.backgroundColor = colors[4]
+        document.getElementById("five").innerHTML = colors[4]
+
+        document.getElementById("six").style.backgroundColor = colors[5]
+        document.getElementById("six").innerHTML = colors[5]
     })
     .catch(error => {
         console.error('SERVER ERROR', error);
@@ -48,6 +78,16 @@ function analogic(hexColor) {
 function clickedContrast() {
     resetIcons(); 
     document.querySelector('#contrast i').style.color = "#4285f4"; 
+}
+
+function clickedTheme() {
+    resetIcons(); 
+    document.querySelector('#theme i').style.color = "#4285f4"; 
+}
+
+function clickedGradient() {
+    resetIcons();
+    document.querySelector('#gradient i').style.color = "#4285f4";
 }
 
 const hexToRgb = function(hex) {
@@ -60,27 +100,6 @@ const hexToRgb = function(hex) {
         }
         : null;
 }
-
-const colorName = function(hex) {
-    const rgb = hexToRgb(hex);
-    let distance = 0;
-    let minDistance = Infinity;
-    let c;
-    
-    colors.forEach(color => {
-        distance = Math.sqrt(
-            (color.rgb.r - rgb.r) ** 2 +
-            (color.rgb.g - rgb.g) ** 2 +
-            (color.rgb.b - rgb.b) ** 2
-        );
-
-        if (distance < minDistance) {
-            minDistance = distance;
-            c = color;
-        }
-    });
-    return c.name;
-};
 
 const pickr = Pickr.create({
     el: '.color-picker',
